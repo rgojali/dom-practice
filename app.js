f1e993dcb0c6fca0f6a1e66fbad5e74957bac9fb
Rusilah Gojali <rgojali@gmail.com>
2018-11-12 18:15:27 -0800

    add index.html app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict';
+//'use strict';
 
 //Changing the color of the box
 /*
@@ -11,6 +11,7 @@ console.log(square[0])
 
 //Changing the text in movie list
 
+/*
 const lis = document.querySelectorAll('.fav-movie')
 
 console.log(lis);
@@ -19,3 +20,58 @@ for (let i = 0; i < lis.length; i++) {
     console.log(lis[i]);
     lis[i].innerHTML += ' (new movie)'
 };
+*/
+/*
+//Using EventListener to change color of square
+const input = document.getElementById('input-color');
+const square = document.querySelector('.square');
+
+//console.log('input:', input);
+//console.log('square:', square);
+
+//Event Handler
+let inputHandler = function(e) {
+    console.log('e:', e);
+    square.style.backgroundColor = e.target.value;
+};
+
+// Event Listener
+input.addEventListener('keyup', inputHandler);
+*/
+
+//Using EventListener to change text
+/*
+const inputText = document.getElementById('input-text');
+const changeText = document.getElementById('change-text');
+
+//console.log('changeText')
+
+let inputTextHandler = (e) => {
+    console.log('e:', e);
+    if (e.keyCode == 13) {
+        changeText.innerHTML = e.target.value;
+    };
+};
+inputText.addEventListener('keyup', inputTextHandler);
+*/
+
+//Using EventListeneres for click events
+
+const btn = document.getElementById('add-movie');
+const inputMovie = document.getElementById('movie-text');
+const ul = document.getElementById('movie-list');
+
+
+let addMovie = (e) => {
+
+    let movie = inputMovie.value;
+
+    let li = document.createElement('li');
+    li.textContent = movie;
+
+    ul.apppendChild(li);
+}
+
+
+
+btn.addEventListener('click', addMovie)
